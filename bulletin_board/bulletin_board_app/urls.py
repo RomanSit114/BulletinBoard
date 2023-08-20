4fbe7fa929d78d0830fd326a4c6af929f1dfbdd7
Roman <romansitdik@gmail.com>
2023-08-20 19:21:13 +0300

    created ad.html

--- bulletin_board/bulletin_board_app/urls.py
+++ bulletin_board/bulletin_board_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AdsList  # импортируем наше представление
+from .views import AdsList, AdsDetailView
 
 
 urlpatterns = [
-    path('', AdsList.as_view()),
+    path('', AdsList.as_view(), name='main_page'),
+    path('<int:pk>', AdsDetailView.as_view(), name='ad'),
 ]
--- bulletin_board/bulletin_board_app/views.py
+++ bulletin_board/bulletin_board_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Ad
 from django.template.loader import render_to_string
 from django.urls import reverse
@@ -9,3 +9,9 @@ class AdsList(ListView):
     model = Ad
     template_name = 'bulletin_board_app/ads.html'
     context_object_name = 'ads'
+
+class AdsDetailView(DetailView):
+    model = Ad
+    template_name = 'bulletin_board_app/ad.html'
+    context_object_name = 'ad'
+    queryset = Ad.objects.all()
