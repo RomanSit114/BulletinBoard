a376285250a668249edb0dc82ed5563ec34130b5
Roman <romansitdik@gmail.com>
2023-08-20 20:20:36 +0300

    added pagination

--- bulletin_board/bulletin_board_app/views.py
+++ bulletin_board/bulletin_board_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Ad
 from django.template.loader import render_to_string
 from django.urls import reverse
@@ -9,9 +9,17 @@ class AdsList(ListView):
     model = Ad
     template_name = 'bulletin_board_app/ads.html'
     context_object_name = 'ads'
+    ordering = ['-id']
+    paginate_by = 10
 
 class AdsDetailView(DetailView):
     model = Ad
     template_name = 'bulletin_board_app/ad.html'
     context_object_name = 'ad'
     queryset = Ad.objects.all()
+
+# class NewsCreateView(PermissionRequiredMixin, CreateView):
+#     permission_required = ('newapp.add_post')
+#     form_class = NewsForm
+#     template_name = 'news_create.html'
+#     success_url = '/news/'
