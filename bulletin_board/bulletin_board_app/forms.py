968bf3b738c3554a7a247d0a4ddcbd0445a8a842
roman <romansitdik@gmail.com>
2023-08-23 13:16:34 +0300

    changed ad_create.html

--- bulletin_board/bulletin_board_app/forms.py
+++ bulletin_board/bulletin_board_app/forms.py
@@ -4,8 +4,4 @@ from .models import Ad
 class AdsForm(ModelForm):
     class Meta:
         model = Ad
-        # exclude = ['author']
-        # fields = ['author', 'title', 'text', 'adCategory']
         fields = ['title', 'text', 'adCategory', 'image', 'file']
-        # fields = ['title', 'text', 'adCategory']
-
